b633f5f7f7bb7ff075507f68298b31f443eb3894
Vraj Desai <desaivraj18@gmail.com>
2026-06-27 12:03:44 +0530

    float value print using a f keyword

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val x : Int = 2
+    val x  = 2f
     println(x)
     }
