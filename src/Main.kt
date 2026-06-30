21aefee70c64ddb0c57de8bb683f345883b709a2
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:57:20 +0530

    console input here we are define number from user using a input = readln function and only take input int using input.toint

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val x = 11
-    val y = 12
-   val areBothEven =  x % 2 == 0 || y % 2 ==0
-    println(areBothEven)// here we are using or operator if one conidion is true then the result is true because it need only one true result if both are true then final result are also true if one is true then also final result is true
-    }
+println("Enter a number:")
+    val input = readln()
+    val inputasInteger = input.toInt()
+
+    val isEven = inputasInteger % 2 == 0
+    println("Is the Number even? $isEven")
+
+}
