576f2321cd9c8584a7e28fca15971a70fbaf4052
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:03:20 +0530

    basic increment operation using var

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val x  = 10
+    var x  = 10
     val y = 20
-
-    val addition = x + y;
-    println(addition)
-
-    val subtraction  = x -y;
-    println(subtraction)
-
-    val division = x / y;
-    println(division)
-
-    val modulo = x % y;
-    println(modulo)
-
-    val multiplication = x * y;
-    println(multiplication)
+    x += 5
+    println(x + y)
     }
