9cffba859de84646209246436c2bec0934eabd7a
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 10:55:37 +0530

    basic arithmetic operation

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var x  = "Hello World!"
-    x = "Bye Bye Hello World!"
-    println(x)
+    val x  = 10
+    val y = 20
+
+    val addition = x + y;
+    println(addition)
+
+    val subtraction  = x -y;
+    println(subtraction)
+
+    val division = x / y;
+    println(division)
+
+    val modulo = x % y;
+    println(modulo)
+
+    val multiplication = x * y;
+    println(multiplication)
     }
