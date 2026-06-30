d6eda396840d92814d78f07a0b8fff9b5ce66468
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:27:16 +0530

    here we comapre a string with another val

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val x  = "hello"
-    val builder = StringBuilder("Hello")
+    var x  = "hello"
+//    x = "Hello"
+    val y = "Hello"
 
-    println(x.contentEquals(builder))// here we are compare specific string with another string using builder method which are compare to x's string
+    println(x.contentEquals(y))// here we are compare specific string with another string using builder method which are compare to x's string
     }
