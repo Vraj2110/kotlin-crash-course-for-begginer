b2122ec23d757c6417e1ed3f35f58fe809e46c8e
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:35:40 +0530

    here we just check is x and y are even numberr after a divide by 2?

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var x  = "hello"
-  x = "Hello"
-    val y = "Hello"
-
-    println(x.contentEquals(y))// here we are compare specific string with another string using builder method which are compare to x's string
+    val x = 11
+    val y = 12
+   println("Is x an even number? ${x % 2 == 0} ")
+    println("Is y an even number? ${y % 2 == 0} ")
     }
