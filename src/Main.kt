4dc4393299503642d0ddf8a9430adeeb0a054046
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:25:26 +0530

    comapre string using a builder and contentequals function

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val x  = 11
-    val y = 11
+    val x  = "hello"
+    val builder = StringBuilder("Hello")
 
-    val areNumbersTheSame = x == y
-    println(areNumbersTheSame)
+    println(x.contentEquals(builder))// here we are compare specific string with another string using builder method which are compare to x's string
     }
