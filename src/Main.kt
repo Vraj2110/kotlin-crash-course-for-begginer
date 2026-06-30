f7d67cc9899ae1c1597a42bc185935374c8b8939
Vraj Desai <desaivraj18@gmail.com>
2026-06-30 11:27:43 +0530

    comapre string using var

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     var x  = "hello"
-//    x = "Hello"
+  x = "Hello"
     val y = "Hello"
 
     println(x.contentEquals(y))// here we are compare specific string with another string using builder method which are compare to x's string
