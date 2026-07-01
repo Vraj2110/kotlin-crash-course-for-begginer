c4d7c7a0664407b140841150359f52a2379b3411
Vraj Desai <desaivraj18@gmail.com>
2026-07-01 23:03:31 +0530

    we use when expression to make a easy to express and understand

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-println("Enter a number:")
+    println("Enter a number:")
     val input = readln()
     val inputasInteger = input.toIntOrNull()
 
-    if (inputasInteger != null){
-        val output =  if(inputasInteger % 2 == 0){
-            "The Number Is Even!"
-        }else{
-            "The Number Is Odd!"
-        }
+    if (inputasInteger != null) {
+        val output = when{
+            inputasInteger % 2 == 0 -> "The Number Is Even\n"
+            else -> "The Number Is Odd\n"
+            }
         println(output)
-    }else{
-        println("Invalid Input Enter a Integer")
+        }else{
+            println("Enter The Valid Value")
     }
-}
+    }
