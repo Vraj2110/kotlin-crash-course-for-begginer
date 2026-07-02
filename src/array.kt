92fc6d90570b031019da93b95e8b027dd7228808
Vraj Desai <desaivraj18@gmail.com>
2026-07-02 22:58:00 +0530

    here we use logical and operator so if user try to access beyond the index then i get message

--- src/array.kt
+++ src/array.kt
@@ -2,9 +2,11 @@ fun main() {
     val input = readln()
     val userinput = input.toIntOrNull()
     val favNumbers = intArrayOf(1, 21, 22, 3, 4, 5)
-    if (userinput != null) {
+    if (userinput != null && userinput<favNumbers.size) {
 
         println("Your Number iS ${favNumbers[userinput]}")
 
+    }else{
+        println("Your index Number is Doesn't exist")
     }
 }
