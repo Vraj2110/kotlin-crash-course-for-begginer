bb07f8a83cddb27e58063ebadbdbcb0ff8d81b3d
Vraj Desai <desaivraj18@gmail.com>
2026-07-02 23:02:41 +0530

    if we enter the beyond the index value now we got message if user enter a -1

--- src/array.kt
+++ src/array.kt
@@ -2,7 +2,7 @@ fun main() {
     val input = readln()
     val userinput = input.toIntOrNull()
     val favNumbers = intArrayOf(1, 21, 22, 3, 4, 5)
-    if (userinput != null && userinput<favNumbers.size) {
+    if (userinput != null && userinput in 0..favNumbers.lastIndex) {
 
         println("Your Number iS ${favNumbers[userinput]}")
 
